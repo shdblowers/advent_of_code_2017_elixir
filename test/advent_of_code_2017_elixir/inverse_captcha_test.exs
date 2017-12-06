35badf39aac0b3750d4841ae3390803cf253b9ec
Steven Blowers <sblowers@findmypast.com>
2017-12-06 17:53:33 +0000

    more tests for day 1

--- test/advent_of_code_2017_elixir/inverse_captcha_test.exs
+++ test/advent_of_code_2017_elixir/inverse_captcha_test.exs
@@ -15,4 +15,16 @@ defmodule AdventOfCode2017Elixir.InverseCaptchaTest do
     assert InverseCaptcha.solve("1122") == 3
   end
 
+  test "solve for all 1s" do
+    assert InverseCaptcha.solve("11111") == 5
+  end
+
+  test "solve for result equalling 0" do
+    assert InverseCaptcha.solve("12345") == 0
+  end
+
+  test "solve intermediate example" do
+    assert InverseCaptcha.solve("91212129") == 9
+  end
+
 end

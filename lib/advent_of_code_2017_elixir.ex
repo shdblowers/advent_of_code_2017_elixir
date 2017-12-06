f563b5cff24ae2eeee14c30fe9e1ef791cabd0dc
Steven Blowers <sblowers@findmypast.com>
2017-12-06 17:09:49 +0000

    mix new advent_of_code_2017_elixir

---
+++ lib/advent_of_code_2017_elixir.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode2017Elixir do
+  @moduledoc """
+  Documentation for AdventOfCode2017Elixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode2017Elixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent_of_code_2017_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule AdventOfCode2017ElixirTest do
+  use ExUnit.Case
+  doctest AdventOfCode2017Elixir
+
+  test "greets the world" do
+    assert AdventOfCode2017Elixir.hello() == :world
+  end
+end
